e3ff8e1f350d76833863cd46a5d1159907719cb2
r4z4 <r4z4aa@gmail.com>
2023-12-29 15:45:50 -0600

    vc

--- test/doi_esper/attachments_test.exs
+++ test/doi_esper/attachments_test.exs
@@ -3,6 +3,14 @@ defmodule DoiEsper.AttachmentsTest do
   alias DoiEsper.Core.Attachment
 
   describe "to_attachment/1" do
+
+    # setup do
+    #   response_as_string =
+    #     File.read!("test/support/weather_api_response.json")
+    #   response_as_map = Jason.decode!(response_as_string)
+    #   %{weather_data: response_as_map}
+    # end
+
     test "success: converts map to attachments struct" do
       object = %{
         "id" => "0aab625c-c034-4066-89f6-095a6fe761a8",
@@ -15,7 +23,20 @@ defmodule DoiEsper.AttachmentsTest do
       attachment = %Attachment{id: "0aab625c-c034-4066-89f6-095a6fe761a8", title: "Title", path: "../title/path", type: 1, data: nil}
 
       assert Attachment.to_attachment(object) == attachment
+    end
 
+    test "error: missing field raises error" do
+      object = %{
+        "id" => "0aab625c-c034-4066-89f6-095a6fe761a8",
+        "title" => "Title",
+        # "path" => "../title/path",
+        "type" => 1,
+        "data" => nil,
+      }
+
+      attachment = %Attachment{id: "0aab625c-c034-4066-89f6-095a6fe761a8", title: "Title", path: "../title/path", type: 1, data: nil}
+
+      assert Attachment.to_attachment(object) == attachment
     end
   end
 end
