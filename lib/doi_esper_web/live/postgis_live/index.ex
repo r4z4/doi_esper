18a21187f5a3cefcd0ae7abfb8d8cf39fa8ad987
r4z4 <r4z4aa@gmail.com>
2023-12-31 18:20:52 -0600

    find nearest start

--- lib/doi_esper_web/live/postgis_live/index.ex
+++ lib/doi_esper_web/live/postgis_live/index.ex
@@ -1,9 +1,40 @@
 defmodule DoiEsperWeb.PostgisLive.Index do
   use DoiEsperWeb, :live_view
   alias DoiEsper.Postgis.PostgisTest
+  alias DoiEsper.Postgis.UsHospitals
   alias DoiEsper.Repo
   import Ecto.Query
   import Geo.PostGIS
+
+  @impl true
+  def handle_event("service_casted", params, socket) do
+    data =
+      case Kernel.elem(Integer.parse(params["id"]), 0) do
+        # GenServer.cast String.to_existing_atom(params["castto"]), {String.to_existing_atom(params["op"]), String.to_existing_atom(params["res"])}
+        1 -> find_nearest("Doesnt Matter What Address")
+        2 -> find_nearest("Doesnt Matter What Address")
+        _ ->
+          find_nearest("Doesnt Matter What Address")
+      end
+    IO.inspect(data, label: "Data")
+    {:noreply,
+      socket
+      |> assign(postgis_data: data)}
+  end
+
+  def fetch_coords() do
+    # FIXME fetch from API
+    {44.458376, -93.161693}
+  end
+
+  def find_nearest(_addr) do
+    coords = fetch_coords()
+    point = %Geo.Point{coordinates: coords, srid: 4326}
+    query = from hospital in UsHospitals, limit: 10, select: hospital.name, where: st_dwithin_in_meters(hospital.geom, ^point, 10000.0)
+    query
+    |> Repo.all
+  end
+
 
   def example_query(geom) do
     # Just override it for now
--- lib/doi_esper_web/postgis_components.ex
+++ lib/doi_esper_web/postgis_components.ex
@@ -1,4 +1,5 @@
 defmodule DoiEsperWeb.PostgisComponents do
+  use DoiEsperWeb, :live_view
   use Phoenix.Component
   # alias Phoenix.LiveView.JS
 
@@ -9,6 +10,19 @@ defmodule DoiEsperWeb.PostgisComponents do
         <%= if @postgis_data do %>
             <p><%= @postgis_data %></p>
         <% end %>
+
+      <button
+        type="button"
+        phx-click="service_casted"
+        phx-value-op={:fetch_resource}
+        phx-value-res={:emojis}
+        phx-value-castto={:consumer_2}
+        phx-value-id={2}
+        value={2}
+        class="inline-block rounded border-2 border-success w-3/3 px-2 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-success transition duration-150 ease-in-out hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 focus:border-success-600 focus:text-success-600 focus:outline-none focus:ring-0 active:border-success-700 active:text-success-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+      >Find Nearest #2
+      </button>
+
       </div>
     """
   end
